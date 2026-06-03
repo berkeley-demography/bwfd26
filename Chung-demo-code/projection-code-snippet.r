61b3d68d895d1ca32217c6ac96e05d967bc653e7
Kylee Hoffman <92068575+kylee-hoffman@users.noreply.github.com>
2026-06-02 18:52:15 -0700

    Update projection-code-snippet.r

--- Chung-demo-code/projection-code-snippet.r
+++ Chung-demo-code/projection-code-snippet.r
@@ -1,6 +1,6 @@
 # read in a bunch of Lx's, Fx's and Kx's from a few countries
 # They're 5-year age groups so let's say our projection step is 5 years
-# I'm assuming the radix is 1. If not, I have to adjust everything by the radix. 
+# I'm assuming the radix is 1. If not, I have to adjust everything by the radix.
 
 library(here)
 
